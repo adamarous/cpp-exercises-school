80a0763432788b10eebb5c4bd291e545a36d77c1
Adam Martinez <149513579+adamarous@users.noreply.github.com>
2023-11-13 01:19:57 +0000

    Working on exercise 3, matrices

--- ex3p128.cpp
+++ ex3p128.cpp
@@ -3,6 +3,65 @@
 using namespace std;
 
 int main() {
-    
+    int months[12][32], total, day, month, checkMonth, checkDay;
+
+    // Preparing month data; up to 30 filled with days, 1 to 2 empty spaces for non-31 days, and the 31st place with the total days
+    for (int i=0; i<12; i++) {
+        total = 0;
+        switch (i) {
+            case 1:
+                for (int d=0; d<28; d++) {
+                    months[i][d] = d+1;
+                    total += 1;
+                }
+                break;
+            case 3:
+                case 5:
+                    case 8:
+                        case 10:
+                            for (int d=0; d<30; d++) {
+                                months[i][d] = d+1;
+                                total += 1;
+                            }
+                            break;
+            default:
+                for (int d=0; d<31; d++) {
+                    months[i][d] = d+1;
+                    total += 1;
+                }
+                break;
+        }
+        months[i][31] = total;
+    }
+
+    // Getting input from user
+    cout << "Introduzca un valor para el dia y el mes: ";
+    cin >> day >> month;
+
+    // Processing input with prepared data
+    for (int i=0; i<12; i++) {
+        if (month == i+1) {
+            for (int d=0; d<30; i++) {
+                if (day == day+1) {
+                    // Pending to produce a sorting algorithm for the months prior to the one selected, to compute the day in the year
+                    cout << "El dia " << day << " del " << i+1 << " es el " << 365 - (months[i-1][30] + months[i][d]);
+                    checkDay = 1;
+                }
+            }
+            checkMonth = 1;
+        }
+    }
+
+    if (checkMonth != 1 && checkDay != 1) {
+        cout << "Los valores introducidos no son correctos.";
+    } else {
+        if (checkMonth != 1) {
+            cout << "El valor del mes es incorrecto.";
+        }
+        if (checkDay != 1) {
+            cout << "El valor del día es incorrecto.";
+        }
+    }
+
     return 0;
 }
