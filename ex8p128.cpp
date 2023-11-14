940db18106e061e7b9d67bccb3c58589b74bb0e8
Adam Martinez <149513579+adamarous@users.noreply.github.com>
2023-11-14 21:50:22 +0000

    Began exercise 8 on arrays

---
+++ ex8p128.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int matrix[2][2];
+
+    // Asking for input
+    std::cout << "Introduzca el valor del elemento ";
+    for (int i=0; i<2; i++) {
+        
+    }
+
+    return 0;
+}
