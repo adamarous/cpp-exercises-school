9fe4492ea9aae54d44f3a3456cff8c6d973773c5
Adam Martinez <149513579+adamarous@users.noreply.github.com>
2023-11-22 16:06:23 +0000

    Update ex6p138.cpp
    Should be done, but I'm working from mobile so testing is pending

--- ex6p138.cpp
+++ ex6p138.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    double num[1000], n;
-    int indexer = 0, exit = 0, average = 0, option, number;
+    double num[1000], n, average = 0.0;
+    int indexer = 0, exit = 0, option, number;
 
     // Populating the array with 0 to avoid memory errors
     for (int i=0; i<1000; i++) {
@@ -16,23 +16,27 @@ int main() {
         std::cout << "3. Mostrar media aritmetica" << std::endl;
         std::cout << "0. Terminar" << std::endl;
 
+        std::cout << std::endl;
+        std::cout << ">> ";
         std::cin >> option;
+        std::cout << std::endl;
         switch (option) {
             case 1:
                 if (indexer <= 1000) {
                     std::cout << "Introduzca un dato real de doble precision: ";
                     std::cin >> n;
                     std::cout << std::endl;
+
                     num[indexer] = n;
                     indexer++;
                 } else {
-                    std::cout << "Se ha alcanzado el maximo de valores introducidos.";
+                    std::cout << "No hay espacio para mas valores." << std::endl << std::endl;
                 }
                 break;
             case 2:
                 if (indexer != 0) {
                     for (int i=0; i<indexer; i++) {
-                        if (i == 0) {
+                        if (i == 0 && indexer > 1) {
                             std::cout << num[i] << ", ";
                         } else {
                             if (i == indexer-1) {
@@ -44,25 +48,30 @@ int main() {
                     }
                     std::cout << std::endl;
                 } else {
-                    std::cout << "No se han introducido datos." << std::endl;
+                    std::cout << "No se han introducido datos." << std::endl << std::endl;
                 }
                 break;
             case 3:
-                if (indexer != 0) {
+                if (indexer > 1) {
                     for (int i=0; i<indexer; i++) {
                         average += num[i];
                     }
                     average /= indexer;
                     std::cout << "La media artimetica es " << average << "." << std::endl;
                 } else {
-                    std::cout << "No se han introducido datos." << std::endl;
+                    if (indexer == 0) {
+                        std::cout << "No se han introducido datos." << std::endl;
+                    } else {
+                        std::cout << "Introduzca mas de un dato." << std::endl;
+                    }
+                    std::cout << std::endl;
                 }
                 break;
             case 0:
                 exit = 1;
                 break;
             default:
-                std::cout << "No se ha introducido una de las opciones ofrecidas." << std::endl;
+                std::cout << "No se ha introducido una de las opciones ofrecidas." << std::endl << std::endl;
         }
     }
 
