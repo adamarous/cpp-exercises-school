2c64d6e31c6ade9d9cc6a46c623e9876689fef6f
Adam Martinez <149513579+adamarous@users.noreply.github.com>
2023-11-23 15:36:51 +0000

    Finished exercise on files

--- ex1p132.cpp
+++ ex1p132.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    
+    std::ofstream numbers("numeros.txt");
+    std::string line;
+    int num = 0;
+
+    do {
+        std::cout << "Introduzca un numero: ";
+        std::cin >> num;
+        if (num != 0) {
+            numbers << num << " ";
+        }
+    } while (num != 0);
+
+    numbers.close();
 
     return 0;
 }
